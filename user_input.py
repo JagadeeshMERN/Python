08a960325539ffd247f1c36de33133e520ae5286
JagadeeshMERN <jagadeeshparasa672@gmail.com>
2026-07-04 22:05:46 +0530

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,51 @@
+#print(2+3)
+'''
+a=5
+b=7
+print(a+b)
+'''
+
+'''
+a=4
+b=8
+c=a+b
+print(a+b)
+'''
+
+'''
+a=float(input("Enter the value of a:"))
+b=float(input("Enter the value of b:"))
+print(a+b)
+'''
+
+'''
+a=str(input("Data1:"))
+b=str(input("Data2:"))
+print(a+b)'''
+
+'''fname=input("Enter your first name:")
+lname=input("Enter your last name:")
+print(fname+" "+lname)'''
+
+'''
+a=complex(input("data1:"))
+b=complex(input("data2:"))
+print(a+b)
+'''
+
+'''
+a=bool(input("data1"))
+b=bool(input("data2"))
+c=bool(input("data3"))
+print(a+b+c)
+'''
+
+'''
+a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("choose the option: 1.add 2.sub 3.mul:"))
+print(a+b)
+print(a-b)
+print(a*b)
+'''
+       
